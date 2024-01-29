0f1e57c2f26639d56e79a80282e6cfeafa79aef7
Malamurka2 <157487127+Malamurka2@users.noreply.github.com>
2024-01-29 09:57:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,8 @@ try:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
 
-# except URLError as e:
-#  streamlit.error()
+except URLError as e:
+  streamlit.error()
   
 # streamlit.write('The user entered ', fruit_choice)
 
@@ -57,7 +57,7 @@ try:
 # streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+# streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
